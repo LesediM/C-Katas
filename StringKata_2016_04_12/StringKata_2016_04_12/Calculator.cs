220e87233878d16fb19ffb784b919ae06b7317f9
LesediM <Lesedi.Motsemme@chillisoft.co.za>
2016-04-12 08:53:50 +0200

    string calculato kata 2016-04-12

---
+++ StringKata_2016_04_12/StringKata_2016_04_12/Calculator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringKata_2016_04_12
+{
+    public class Calculator
+    {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+                return 0;
+
+            return GetNumberListSum(numbers);
+        }
+
+        private int GetNumberListSum(string numbers)
+        {
+            var delimiList = GetDelimitersFromNumberString(ref numbers);
+
+            int[] numberlist = GetIntegerNumberList(numbers, delimiList).Where(x => x <= 1000).ToArray();
+            NegativeNumberCheck(numberlist);
+
+            return numberlist.Sum();
+        }
+
+        private IEnumerable<int> GetIntegerNumberList(string numbers, List<char> delimiList)
+        {
+            return numbers.Split(delimiList.ToArray()).Select(ConvertStringToNumber);
+        }
+
+        private void NegativeNumberCheck(int[] numberlist)
+        {
+            int[] negativelist = numberlist.Where(x => x < 0).ToArray();
+            if (negativelist.Any())
+            {
+                throw new ArgumentException("negatives not allowed: " + string.Join(",", negativelist.ToArray()));
+            }
+        }
+
+        private List<char> GetDelimitersFromNumberString(ref string numbers)
+        {
+            List<char> delimiList = new List<char>() { ',', '\n' };
+
+            if (numbers.Contains("//"))
+            {
+                delimiList.AddRange(GetMultipleDeilimtersFromString(numbers));
+            }
+            return delimiList;
+        }
+
+        private char[] GetMultipleDeilimtersFromString(string numbers)
+        {
+            return numbers.Substring(2).ToArray().TakeWhile((x, y) => !ConvertableToInteger(x.ToString(), out y)).Select(x => Convert.ToChar(x)).ToArray();
+        }
+
+        private int ConvertStringToNumber(string x)
+        {
+            int y;
+            return ConvertableToInteger(x, out y) ? y : 0;
+        }
+
+        private bool ConvertableToInteger(string x, out int y)
+        {
+            return int.TryParse(x, out y);
+        }
+    }
+}
---
+++ StringKata_2016_04_12/StringKata_2016_04_12/TestCalculator.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace StringKata_2016_04_12
+{
+    [TestFixture]
+    public class TestCalculator
+    {
+        [Test]
+        public void Add_GivenEmptyString_ShouldReturn0()
+        {
+            var expected = 0;
+            var numbers = "";
+
+            var result = CreateCalculator().Add(numbers);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Add_GivenStringWith1_ShouldReturn1()
+        {
+            var expected = 1;
+            var numbers = "1";
+
+            var result = CreateCalculator().Add(numbers);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Add_GivenStringWith2Numbers_ShouldReturn3()
+        {
+            var expected = 3;
+            var numbers = "1,2";
+
+            var result = CreateCalculator().Add(numbers);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Add_GivenStringWithNewline_ShouldReturn6()
+        {
+            var expected = 6;
+            var numbers = "1\n2,3";
+
+            var result = CreateCalculator().Add(numbers);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Add_GivenStringWithNewDelimiter_ShouldReturn6()
+        {
+            var expected = 6;
+            var numbers = "//;1;2,3";
+
+            var result = CreateCalculator().Add(numbers);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Add_GivenStringWithNegative_ShouldReturnException()
+        {
+            var expected = "negatives not allowed: -2";
+            var numbers = "1,-2,3";
+
+            var result = Assert.Throws<ArgumentException>(()=> CreateCalculator().Add(numbers));
+            Assert.AreEqual(expected, result.Message);
+        }
+
+        [Test]
+        public void Add_GivenStringWith1001_ShouldReturn6()
+        {
+            var expected = 6;
+            var numbers = "1,2,3,1001";
+
+            var result = CreateCalculator().Add(numbers);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Add_GivenStringWithLongDelimiter_ShouldReturn6()
+        {
+            var expected = 6;
+            var numbers = "//***1***2***3";
+
+            var result = CreateCalculator().Add(numbers);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Add_GivenStringWithMultipleDelimiters_ShouldReturn6()
+        {
+            var expected = 6;
+            var numbers = "//*%1%2*3";
+
+            var result = CreateCalculator().Add(numbers);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        private Calculator CreateCalculator()
+        {
+            return new Calculator();
+        }
+    }
+}
