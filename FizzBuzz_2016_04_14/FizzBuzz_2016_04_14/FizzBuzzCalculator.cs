05be86d5a2f44a2fd6b273ab9b4c0e492d3ff4b4
LesediM <Lesedi.Motsemme@chillisoft.co.za>
2016-04-14 08:30:43 +0200

    FizzBuzz Kata 2014-04-14

---
+++ FizzBuzz_2016_04_14/FizzBuzz_2016_04_14/FizzBuzzCalculator.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace FizzBuzz_2016_04_14
+{
+    public class FizzBuzzCalculator
+    {
+        public string GetFizzBuzz(int number)
+        {
+            var sb = new StringBuilder();
+            if (number % 3 == 0)
+                sb.Append("Fizz");
+            if (number % 5 == 0)
+                sb.Append("Buzz");
+            return sb.Length == 0 ? number.ToString() : sb.ToString();
+        }
+    }
+}
---
+++ FizzBuzz_2016_04_14/FizzBuzz_2016_04_14/TestFizzBuzzCalculator.cs
@@ -0,0 +1,152 @@
+﻿using NUnit.Framework;
+
+namespace FizzBuzz_2016_04_14
+{
+    [TestFixture]
+    public class TestFizzBuzzCalculator
+    {
+        [Test]
+        public void GetFizzBuzz_Given1_ShouldReturn1()
+        {
+            var expected = "1";
+            var number = 1;
+            var fizzBuzzCalculator = new FizzBuzzCalculator();
+
+            var result = fizzBuzzCalculator.GetFizzBuzz(number);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void GetFizzBuzz_Given2_ShouldReturn2()
+        {
+            var expected = "2";
+            var number = 2;
+            var fizzBuzzCalculator = new FizzBuzzCalculator();
+
+            var result = fizzBuzzCalculator.GetFizzBuzz(number);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void GetFizzBuzz_Given3_ShouldReturnFizz()
+        {
+            var expected = "Fizz";
+            var number = 3;
+            var fizzBuzzCalculator = new FizzBuzzCalculator();
+
+            var result = fizzBuzzCalculator.GetFizzBuzz(number);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void GetFizzBuzz_Given4_ShouldReturn4()
+        {
+            var expected = "4";
+            var number = 4;
+            var fizzBuzzCalculator = new FizzBuzzCalculator();
+
+            var result = fizzBuzzCalculator.GetFizzBuzz(number);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void GetFizzBuzz_Given5_ShouldReturnBuzz()
+        {
+            var expected = "Buzz";
+            var number = 5;
+            var fizzBuzzCalculator = new FizzBuzzCalculator();
+
+            var result = fizzBuzzCalculator.GetFizzBuzz(number);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void GetFizzBuzz_Given6_ShouldReturnFizz()
+        {
+            var expected = "Fizz";
+            var number = 6;
+            var fizzBuzzCalculator = new FizzBuzzCalculator();
+
+            var result = fizzBuzzCalculator.GetFizzBuzz(number);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void GetFizzBuzz_Given9_ShouldReturnFizz()
+        {
+            var expected = "Fizz";
+            var number = 9;
+            var fizzBuzzCalculator = new FizzBuzzCalculator();
+
+            var result = fizzBuzzCalculator.GetFizzBuzz(number);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void GetFizzBuzz_Given10_ShouldReturnBuzz()
+        {
+            var expected = "Buzz";
+            var number = 10;
+            var fizzBuzzCalculator = new FizzBuzzCalculator();
+
+            var result = fizzBuzzCalculator.GetFizzBuzz(number);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void GetFizzBuzz_Given15_ShouldReturnFizzBuzz()
+        {
+            var expected = "FizzBuzz";
+            var number = 15;
+            var fizzBuzzCalculator = new FizzBuzzCalculator();
+
+            var result = fizzBuzzCalculator.GetFizzBuzz(number);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void GetFizzBuzz_Given20_ShouldReturnBuzz()
+        {
+            var expected = "Buzz";
+            var number = 20;
+            var fizzBuzzCalculator = new FizzBuzzCalculator();
+
+            var result = fizzBuzzCalculator.GetFizzBuzz(number);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void GetFizzBuzz_Given30_ShouldReturnFizzBuzz()
+        {
+            var expected = "FizzBuzz";
+            var number = 30;
+            var fizzBuzzCalculator = new FizzBuzzCalculator();
+
+            var result = fizzBuzzCalculator.GetFizzBuzz(number);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void GetFizzBuzz_Given75_ShouldReturnFizzBuzz()
+        {
+            var expected = "FizzBuzz";
+            var number = 75;
+            var fizzBuzzCalculator = new FizzBuzzCalculator();
+
+            var result = fizzBuzzCalculator.GetFizzBuzz(number);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
