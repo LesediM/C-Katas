277f056082b017beb01ed52c29b44385c6a054dc
LesediM <Lesedi.Motsemme@chillisoft.co.za>
2016-04-18 09:21:45 +0200

    South African ID Kata 2016-04-18

---
+++ SouthAfricanIDNumberKata_2016_04_18/SouthAfricanIDNumberKata_2016_04_18/IDNumberValidator.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace SouthAfricanIDNumberKata_2016_04_18
+{
+    public class IDNumberValidator
+    {
+        public string ExtractIDParts(string idnumber)
+        {
+            var dateofbirth = ExtractDateOfBirth(idnumber);
+            var gender = ExtractGender(idnumber);
+            var citizenship = ExtractCitizenship(idnumber);
+            return $"{dateofbirth}, {gender} {citizenship}";
+        }
+
+        public string ExtractDateOfBirth(string idnumber)
+        {
+            var yy = idnumber.Substring(0, 2);
+            var yyyy = Convert.ToInt32(yy) < 20 ? "20" + yy : "19" + yy;
+            var mm = idnumber.Substring(2, 2);
+            var dd = idnumber.Substring(4, 2);
+
+            return $"{dd}-{mm}-{yyyy}";
+        }
+
+        public string ExtractGender(string idnumber)
+        {
+            var g = Convert.ToInt32(idnumber.Substring(6, 1));
+            var gender = g < 5 ? "Female" : "Male";
+            return gender;
+        }
+
+        public string ExtractCitizenship(string idnumber)
+        {
+            var c = Convert.ToInt32(idnumber.Substring(10, 1));
+            var citizenship = c < 1 ? "SA" : "Other";
+            return citizenship;
+        }
+
+        public int SumOddPositionNumbers(string idnumber)
+        {
+            var oddpositionnumbers = GetNumbersFromIDNumber(idnumber, 0);
+
+            return oddpositionnumbers.Sum(x => Convert.ToInt32(x.ToString()));
+        }
+
+        private List<char> GetNumbersFromIDNumber(string idnumber, int startindex)
+        {
+            var numbers = new List<char>();
+            for (int i = startindex; i < idnumber.Length - 1; i += 2)
+                numbers.Add(idnumber[i]);
+            return numbers;
+        }
+
+        public int GetEvenPositionNumbersSumX2(string idnumber)
+        {
+            var evenpositionnumbers = GetNumbersFromIDNumber(idnumber, 1);
+            return Convert.ToInt32(string.Join("", evenpositionnumbers.Select(x => x.ToString()))) * 2;
+        }
+
+        public int SumEvenPositionResults(string idnumber)
+        {
+            return GetEvenPositionNumbersSumX2(idnumber).ToString().Sum(x => Convert.ToInt32(x.ToString()));
+        }
+
+        public bool ValidateID(string idnumber)
+        {
+            var sumoddpositionnumbers = SumOddPositionNumbers(idnumber);
+            var sumevenpositionnumbers = SumEvenPositionResults(idnumber);
+            var sumnumbers = sumoddpositionnumbers + sumevenpositionnumbers;
+            var tally = (10 - (Convert.ToInt32(sumnumbers.ToString().Last().ToString()))).ToString();
+            var controlnumber = idnumber.Last().ToString();
+
+            return Equals(tally, controlnumber);
+        }
+    }
+}
---
+++ SouthAfricanIDNumberKata_2016_04_18/SouthAfricanIDNumberKata_2016_04_18/TestIDNumberValidator.cs
@@ -0,0 +1,141 @@
+﻿using NUnit.Framework;
+
+namespace SouthAfricanIDNumberKata_2016_04_18
+{
+    [TestFixture]
+    public class TestIDNumberValidator
+    {
+        [Test]
+        public void ExtractIDParts_GivenIDNumber_ShouldReturnIDParts()
+        {
+            //---------------Set up test pack-------------------
+            var idnumber = "8001015009087";
+            var expected = "01-01-1980, Male SA";
+            var idnumbervalidator = new IDNumberValidator();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = idnumbervalidator.ExtractIDParts(idnumber);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void ExtractDateOfBirth_GivenIDNumber_ShouldReturnDateOfBirth()
+        {
+            //---------------Set up test pack-------------------
+            var idnumber = "8001015009087";
+            var expected = "01-01-1980";
+            var idnumbervalidator = new IDNumberValidator();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = idnumbervalidator.ExtractDateOfBirth(idnumber);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void ExtractGender_GivenIDNumber_ShouldReturnGender()
+        {
+            //---------------Set up test pack-------------------
+            var idnumber = "8001015009087";
+            var expected = "Male";
+            var idnumbervalidator = new IDNumberValidator();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = idnumbervalidator.ExtractGender(idnumber);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void ExtractCitizenship_GivenIDNumber_ShouldReturnCitizenship()
+        {
+            //---------------Set up test pack-------------------
+            var idnumber = "8001015009087";
+            var expected = "SA";
+            var idnumbervalidator = new IDNumberValidator();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = idnumbervalidator.ExtractCitizenship(idnumber);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void SumOddPositionNumbers_GivenIDNumber_ShouldReturnSum()
+        {
+            //---------------Set up test pack-------------------
+            var idnumber = "8001015009087";
+            var expected = 13;
+            var idnumbervalidator = new IDNumberValidator();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = idnumbervalidator.SumOddPositionNumbers(idnumber);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void GetEvenPositionNumbers_GivenIDNumber_ShouldReturnSumX2()
+        {
+            //---------------Set up test pack-------------------
+            var idnumber = "8001015009087";
+            var expected = 22196;
+            var idnumbervalidator = new IDNumberValidator();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = idnumbervalidator.GetEvenPositionNumbersSumX2(idnumber);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void SumEvenPositionResults_GivenIDNumber_ShouldReturnSum()
+        {
+            //---------------Set up test pack-------------------
+            var idnumber = "8001015009087";
+            var expected = 20;
+            var idnumbervalidator = new IDNumberValidator();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = idnumbervalidator.SumEvenPositionResults(idnumber);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void ValidateIDNumber_GivenIDNumber_ShouldReturnTrue()
+        {
+            //---------------Set up test pack-------------------
+            var idnumber = "8001015009087";
+            var idnumbervalidator = new IDNumberValidator();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = idnumbervalidator.ValidateID(idnumber);
+            //---------------Test Result -----------------------
+            Assert.IsTrue(results);
+        }
+
+        [Test]
+        public void ValidateIDNumber_Given8013155009082_ShouldReturnTrue()
+        {
+            //---------------Set up test pack-------------------
+            var idnumber = "8013155009082";
+            var idnumbervalidator = new IDNumberValidator();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = idnumbervalidator.ValidateID(idnumber);
+            //---------------Test Result -----------------------
+            Assert.IsTrue(results);
+        }
+    }
+}
