466463757b3ff4f97fa4ae7c250781a2f27e4cb7
LesediM <Lesedi.Motsemme@chillisoft.co.za>
2016-04-22 08:24:20 +0200

    FizzBuzz Kata 2016-04-22

---
+++ FizzBuzz_2016_04_22/FizzBuzz_2016_04_22/FizzBuzz.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace FizzBuzz_2016_04_22
+{
+    public class FizzBuzz
+    {
+        public string Calculate(int number)
+        {
+            var fizzBuzzString = GetFizzBuzzString(number);
+            if (string.IsNullOrEmpty(fizzBuzzString))
+                return $"{number}";
+            return fizzBuzzString;
+        }
+
+        private string GetFizzBuzzString(int number)
+        {
+            var fizzBuzzString = "";
+            fizzBuzzString += CheckIfDivisible(number, 3) ? "Fizz" : "";
+            fizzBuzzString += CheckIfDivisible(number, 5) ? "Buzz" : "";
+            return fizzBuzzString;
+        }
+
+        private bool CheckIfDivisible(int number, int denominator)
+        {
+            return number % denominator == 0;
+        }
+    }
+}
---
+++ FizzBuzz_2016_04_22/FizzBuzz_2016_04_22/TestFizzBuzz.cs
@@ -0,0 +1,182 @@
+﻿using NUnit.Framework;
+
+namespace FizzBuzz_2016_04_22
+{
+    [TestFixture]
+    public class TestFizzBuzz
+    {
+        [Test]
+        public void Calculate_GivenNumber1_ShouldReturn1()
+        {
+            //---------------Set up test pack-------------------
+            var expected = "1";
+            var number = 1;
+            var fizzBuzz = CreateFizzBuzz();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = fizzBuzz.Calculate(number);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+        [Test]
+        public void Calculate_GivenNumber2_ShouldReturn2()
+        {
+            //---------------Set up test pack-------------------
+            var expected = "2";
+            var number = 2;
+            var fizzBuzz = CreateFizzBuzz();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = fizzBuzz.Calculate(number);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+        [Test]
+        public void Calculate_GivenNumber3_ShouldReturnFizz()
+        {
+            //---------------Set up test pack-------------------
+            var expected = "Fizz";
+            var number = 3;
+            var fizzBuzz = CreateFizzBuzz();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = fizzBuzz.Calculate(number);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+        [Test]
+        public void Calculate_GivenNumber4_ShouldReturn4()
+        {
+            //---------------Set up test pack-------------------
+            var expected = "4";
+            var number = 4;
+            var fizzBuzz = CreateFizzBuzz();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = fizzBuzz.Calculate(number);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+        [Test]
+        public void Calculate_GivenNumber5_ShouldReturnBuzz()
+        {
+            //---------------Set up test pack-------------------
+            var expected = "Buzz";
+            var number = 5;
+            var fizzBuzz = CreateFizzBuzz();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = fizzBuzz.Calculate(number);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+        [Test]
+        public void Calculate_GivenNumber6_ShouldReturnFizz()
+        {
+            //---------------Set up test pack-------------------
+            var expected = "Fizz";
+            var number = 6;
+            var fizzBuzz = CreateFizzBuzz();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = fizzBuzz.Calculate(number);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+        [Test]
+        public void Calculate_GivenNumber9_ShouldReturnFizz()
+        {
+            //---------------Set up test pack-------------------
+            var expected = "Fizz";
+            var number = 9;
+            var fizzBuzz = CreateFizzBuzz();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = fizzBuzz.Calculate(number);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+        [Test]
+        public void Calculate_GivenNumber10_ShouldReturnBuzz()
+        {
+            //---------------Set up test pack-------------------
+            var expected = "Buzz";
+            var number = 10;
+            var fizzBuzz = CreateFizzBuzz();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = fizzBuzz.Calculate(number);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+        [Test]
+        public void Calculate_GivenNumber15_ShouldReturnFizzBuzz()
+        {
+            //---------------Set up test pack-------------------
+            var expected = "FizzBuzz";
+            var number = 15;
+            var fizzBuzz = CreateFizzBuzz();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = fizzBuzz.Calculate(number);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+        [Test]
+        public void Calculate_GivenNumber20_ShouldReturnBuzz()
+        {
+            //---------------Set up test pack-------------------
+            var expected = "Buzz";
+            var number = 20;
+            var fizzBuzz = CreateFizzBuzz();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = fizzBuzz.Calculate(number);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+        [Test]
+        public void Calculate_GivenNumber30_ShouldReturnFizzBuzz()
+        {
+            //---------------Set up test pack-------------------
+            var expected = "FizzBuzz";
+            var number = 30;
+            var fizzBuzz = CreateFizzBuzz();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = fizzBuzz.Calculate(number);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+        [Test]
+        public void Calculate_GivenNumber75_ShouldReturnFizzBuzz()
+        {
+            //---------------Set up test pack-------------------
+            var expected = "FizzBuzz";
+            var number = 75;
+            var fizzBuzz = CreateFizzBuzz();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = fizzBuzz.Calculate(number);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+
+        private FizzBuzz CreateFizzBuzz()
+        {
+            return new FizzBuzz();
+        }
+    }
+}
