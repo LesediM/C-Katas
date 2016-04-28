16a9439f16042c0de0c8224fb9f073d399951f79
LesediM <Lesedi.Motsemme@chillisoft.co.za>
2016-04-28 09:06:04 +0200

    South African IDNumber Kata 2016-04-28

---
+++ SouthAfricanIDNumberKata_2016_04_28/SouthAfricanIDNumberKata_2016_04_28/SaIdNumberValidator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SouthAfricanIDNumberKata_2016_04_28
+{
+    public class SaIdNumberValidator
+    {
+        public int AddOddPositionDigits(string idNumber)
+        {
+            var listNumbers = GetNumbers(idNumber, 0, idNumber.Length - 1);
+            return listNumbers.Sum(x => int.Parse(x));
+        }
+
+        private List<string> GetNumbers(string idNumber, int startPosition, int endPosition)
+        {
+            var listNumbers = new List<string>();
+            for (int i = startPosition; i < endPosition; i += 2)
+            {
+                listNumbers.Add(idNumber[i].ToString());
+            }
+            return listNumbers;
+        }
+
+        public int GetEvenPositionDigits(string idNumber)
+        {
+            var listNumbers = GetNumbers(idNumber, 1, idNumber.Length);
+            return int.Parse(string.Concat(listNumbers.ToArray())) * 2;
+        }
+
+        public int AddEvenPositionDigits(string idNumber)
+        {
+            var evenPositionDigits = GetEvenPositionDigits(idNumber);
+            return evenPositionDigits.ToString().Sum(x => int.Parse(x.ToString()));
+        }
+
+        public int ResolvePositionDigits(string idNumber)
+        {
+            var oddPositionDigits = AddOddPositionDigits(idNumber);
+            var evenPositionDigits = AddEvenPositionDigits(idNumber);
+            return oddPositionDigits + evenPositionDigits;
+        }
+
+        public int ResolveTallyNumber(string idNumber)
+        {
+            var results = ResolvePositionDigits(idNumber).ToString();
+            var secondDigit = results.Length > 1 ? int.Parse(results[1].ToString()) : int.Parse(results);
+            return 10 - secondDigit;
+        }
+
+        public bool ValidateID(string idNumber)
+        {
+            var controlDigit = int.Parse(idNumber.Last().ToString());
+            var tallyNumber = ResolveTallyNumber(idNumber);
+            return Equals(tallyNumber, controlDigit);
+        }
+    }
+}
---
+++ SouthAfricanIDNumberKata_2016_04_28/SouthAfricanIDNumberKata_2016_04_28/TestSaIdNumberValidator.cs
@@ -0,0 +1,93 @@
+﻿using NUnit.Framework;
+
+namespace SouthAfricanIDNumberKata_2016_04_28
+{
+    [TestFixture]
+    public class TestSaIdNumberValidator
+    {
+        [Test]
+        public void AddOddPositionDigits_GivenIDNumber_ShouldSumOddPositionDigitsExceptLast()
+        {
+            //---------------Set up test pack-------------------
+            var expected = 13;
+            var idNumber = "8001015009087";
+            var saIdNumberValidator = CreateSaIdNumberValidator();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = saIdNumberValidator.AddOddPositionDigits(idNumber);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+        [Test]
+        public void GetEvenPositionDigits_GivenIDNumber_ShouldReturnConcatedEvenPositionDigitsThenDoubledResult()
+        {
+            //---------------Set up test pack-------------------
+            var expected = 22196;
+            var idNumber = "8001015009087";
+            var saIdNumberValidator = CreateSaIdNumberValidator();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = saIdNumberValidator.GetEvenPositionDigits(idNumber);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+        [Test]
+        public void AddEvenPositionDigits_GivenIDNumber_ShouldSumEvenPositionDigits()
+        {
+            //---------------Set up test pack-------------------
+            var expected = 20;
+            var idNumber = "8001015009087";
+            var saIdNumberValidator = CreateSaIdNumberValidator();
+            //---------------Assert Precondition----------------
+
+            var results = saIdNumberValidator.AddEvenPositionDigits(idNumber);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+        [Test]
+        public void ResolvePositionDigits_GivenIDNumber_ShouldSumOddAndEvenPositionDigitsResults()
+        {
+            //---------------Set up test pack-------------------
+            var expected = 33;
+            var idNumber = "8001015009087";
+            var saIdNumberValidator = CreateSaIdNumberValidator();
+            //---------------Assert Precondition----------------
+
+            var results = saIdNumberValidator.ResolvePositionDigits(idNumber);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+        [Test]
+        public void ResolveTallyNumber_GivenIDNumber_ShouldSubtract2ndDigitOfResolvePositionDigitsFromTenAndReturnSecondDigitOfResults()
+        {
+            //---------------Set up test pack-------------------
+            var expected = 7;
+            var idNumber = "8001015009087";
+            var saIdNumberValidator = CreateSaIdNumberValidator();
+            //---------------Assert Precondition----------------
+
+            var results = saIdNumberValidator.ResolveTallyNumber(idNumber);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+        [Test]
+        public void ValidateID_GivenIDNumber_ShouldReturnTrueIfIdNumberIsValid()
+        {
+            //---------------Set up test pack-------------------
+            var idNumber = "8001015009087";
+            var saIdNumberValidator = CreateSaIdNumberValidator();
+            //---------------Assert Precondition----------------
+
+            var results = saIdNumberValidator.ValidateID(idNumber);
+            //---------------Test Result -----------------------
+            Assert.IsTrue(results);
+        }
+
+        private SaIdNumberValidator CreateSaIdNumberValidator()
+        {
+            return new SaIdNumberValidator();
+        }
+    }
+}
