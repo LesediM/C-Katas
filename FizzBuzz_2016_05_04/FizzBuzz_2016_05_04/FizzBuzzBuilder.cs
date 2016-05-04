f3f4f6b8b6b8dddefd2508d282ea10f46bce821a
LesediM <Lesedi.Motsemme@chillisoft.co.za>
2016-05-04 08:58:58 +0200

    FizzBuzz Kata 2016-05-04

---
+++ FizzBuzz_2016_05_04/FizzBuzz_2016_05_04/FizzBuzzBuilder.cs
@@ -0,0 +1,25 @@
+﻿namespace FizzBuzz_2016_05_04
+{
+    public class FizzBuzzBuilder
+    {
+        public string Build(int input)
+        {
+            var numberIsDivisibleByThree = input % 3 == 0;
+            var numberIsDivisibleByFive = input % 5 == 0;
+
+            if (!(numberIsDivisibleByThree || numberIsDivisibleByFive))
+                return input.ToString();
+
+            var fizzBuzzBuild = GetFizzBuzzBuild(numberIsDivisibleByThree, numberIsDivisibleByFive);
+            return fizzBuzzBuild;
+        }
+
+        private string GetFizzBuzzBuild(bool numberIsDivisibleByThree, bool numberIsDivisibleByFive)
+        {
+            var fizzBuzzBuild = "";
+            fizzBuzzBuild += numberIsDivisibleByThree ? "Fizz" : "";
+            fizzBuzzBuild += numberIsDivisibleByFive ? "Buzz" : "";
+            return fizzBuzzBuild;
+        }
+    }
+}
---
+++ FizzBuzz_2016_05_04/FizzBuzz_2016_05_04/TestFizzBuzzBuilder.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+namespace FizzBuzz_2016_05_04
+{
+    [TestFixture]
+    public class TestFizzBuzzBuilder
+    {
+        [TestCase(1, "1")]
+        [TestCase(2, "2")]
+        [TestCase(3, "Fizz")]
+        [TestCase(4, "4")]
+        [TestCase(5, "Buzz")]
+        [TestCase(6, "Fizz")]
+        [TestCase(9, "Fizz")]
+        [TestCase(10, "Buzz")]
+        [TestCase(15, "FizzBuzz")]
+        [TestCase(20, "Buzz")]
+        [TestCase(30, "FizzBuzz")]
+        [TestCase(75, "FizzBuzz")]
+        public void Build_GivenInput_ShouldReturnResult(int input, string expected)
+        {
+            //---------------Set up test pack-------------------
+            var fizzBuzzBuilder = CreateFizzBuzzBuilder();
+            //---------------Assert Precondition----------------
+            //---------------Execute Test ----------------------
+            var results = fizzBuzzBuilder.Build(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+
+        private FizzBuzzBuilder CreateFizzBuzzBuilder()
+        {
+            return new FizzBuzzBuilder();
+        }
+    }
+}
