f4b52fbe44ad31e11d68116487fcde40582c37a5
LesediM <Lesedi.Motsemme@chillisoft.co.za>
2016-05-09 08:26:12 +0200

    String Calculator Kata 2016-05-09

---
+++ StringCalculator_2016_05_09/StringCalculator_2016_05_09/StringCalculator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StringCalculator_2016_05_09
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+                return 0;
+
+            var delimiters = new[] { ',', '\n' };
+            numbers = GetDelimiters(numbers);
+            var numberlist = SplitNumberlist(numbers, delimiters);
+            NegativeNumberCheck(numberlist);
+            return numberlist.Sum(x => int.Parse(x.ToString()));
+        }
+
+        private static string GetDelimiters(string numbers)
+        {
+            if (numbers.StartsWith("//"))
+            {
+                var splitNumberString = numbers.Split('\n');
+                var delimiterList = splitNumberString[0].Replace("//", "")
+                    .Split(new[] {'[', ']'}, StringSplitOptions.RemoveEmptyEntries);
+                var newNumberString = splitNumberString[1];
+                foreach (var delimiter in delimiterList)
+                {
+                    newNumberString = newNumberString.Replace(delimiter, ",");
+                }
+                numbers = newNumberString;
+            }
+            return numbers;
+        }
+
+        private string[] SplitNumberlist(string numbers, char[] delimiters)
+        {
+            return numbers.Split(delimiters).Where(x => int.Parse(x) <= 1000).ToArray();
+        }
+
+        private void NegativeNumberCheck(string[] numberlist)
+        {
+            var negativenumbers = numberlist.Where(x => int.Parse(x.ToString()) < 0);
+            if (negativenumbers.Any())
+                throw new ArgumentException("negatives not allowed: " + string.Join(",", negativenumbers.ToArray()));
+        }
+    }
+}
