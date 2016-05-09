59b3270e590fd3888ac61caeab578e994cb7bf65
LesediM <Lesedi.Motsemme@chillisoft.co.za>
2016-05-09 09:09:29 +0200

    South African Idnumber Kata 2016-05-09

---
+++ SouthAfricanIDNumberKata_2016_05_09/SouthAfricanIDNumberKata_2016_05_09/SaIdNumberExtractor.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.AccessControl;
+
+namespace SouthAfricanIDNumberKata_2016_05_09
+{
+    public class SaIdNumberExtractor
+    {
+        public string ExtractDateOfBirth(string idNumber)
+        {
+            var yy = int.Parse(idNumber.Substring(0, 2));
+            var prefix = yy < 20 ? "20" : "19";
+            var yyyy = string.Concat(prefix, yy);
+            var mm = idNumber.Substring(2, 2);
+            var dd = idNumber.Substring(4, 2);
+            return $"{dd}-{mm}-{yyyy}";
+        }
+
+        public string ExtractGender(string idNumber)
+        {
+            var g = int.Parse(idNumber.Substring(6, 1));
+            var gender = g < 5 ? "Female" : "Male";
+            return gender;
+        }
+
+        public string ExtractCitizenshipStatus(string idNumber)
+        {
+            var c = int.Parse(idNumber.Substring(10, 1));
+            var citizenshipstatus = c < 1 ? "SA" : "Other";
+            return citizenshipstatus;
+        }
+
+        public string ExtractIDParts(string idNumber)
+        {
+            var dateOfBirth = ExtractDateOfBirth(idNumber);
+            var gender = ExtractGender(idNumber);
+            var citizenshipStatus = ExtractCitizenshipStatus(idNumber);
+            return $"{dateOfBirth},{gender},{citizenshipStatus}";
+        }
+    }
+}
---
+++ SouthAfricanIDNumberKata_2016_05_09/SouthAfricanIDNumberKata_2016_05_09/SaIdNumberValidator.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SouthAfricanIDNumberKata_2016_05_09
+{
+    public class SaIdNumberValidator
+    {
+        public int AddOddPositionDigits(string idNumber)
+        {
+            var numberList = GetNumberList(idNumber, 0);
+            return numberList.Sum();
+        }
+
+        private List<int> GetNumberList(string idNumber, int startPostion)
+        {
+            var numberList = new List<int>();
+            for (int i = startPostion; i < idNumber.Length - 1; i += 2)
+            {
+                numberList.Add(int.Parse(idNumber[i].ToString()));
+            }
+            return numberList;
+        }
+
+        public int GetEvenPositionDigits(string idNumber)
+        {
+            var numberList = GetNumberList(idNumber, 1);
+            var results = int.Parse(string.Concat(numberList)) * 2;
+            return results;
+        }
+
+        public int AddEvenPositionDigits(string idNumber)
+        {
+            var evenPositionDigits = GetEvenPositionDigits(idNumber);
+            var sum = evenPositionDigits.ToString().Sum(x => int.Parse(x.ToString()));
+            return sum;
+        }
+
+        public int AddPositionDigitCalculations(string idNumber)
+        {
+            var oddPositionDigits = AddOddPositionDigits(idNumber);
+            var evenPositionDigits = AddEvenPositionDigits(idNumber);
+            var results = oddPositionDigits + evenPositionDigits;
+            return results;
+        }
+
+        public int GetTallyNumber(string idNumber)
+        {
+            var digitCalculations = AddPositionDigitCalculations(idNumber).ToString();
+            var numberForSubtraction = digitCalculations.Length > 1 ? int.Parse(digitCalculations[1].ToString()) : int.Parse(digitCalculations);
+            var tallyNumber = 10 - numberForSubtraction;
+            return tallyNumber;
+        }
+
+        public bool ValidateID(string idNumber)
+        {
+            var tallyNumber = GetTallyNumber(idNumber).ToString();
+            var controlDigit = idNumber.Last().ToString();
+            return string.Equals(controlDigit, tallyNumber);
+        }
+    }
+}
