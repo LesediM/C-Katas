e48a06be24baff4f9f0d05f1948e09b13cab8c0e
LesediM <Lesedi.Motsemme@chillisoft.co.za>
2016-05-11 13:54:55 +0200

    Completed Fizz Buzz 2016-05-10

--- FizzBuzz_2016_05_10/FizzBuzz_2016_05_10/FizzBuzzBuilder.cs
+++ FizzBuzz_2016_05_10/FizzBuzz_2016_05_10/FizzBuzzBuilder.cs
@@ -4,9 +4,17 @@ namespace FizzBuzz_2016_05_10
     {
         public string Build(int input)
         {
-            if (input % 3 == 0)
-                return "Fizz";
-            return input.ToString();
+            var divisibleBy3 = input % 3 == 0;
+            var divisibleBy5 = input % 5 == 0;
+
+            if (!(divisibleBy3 | divisibleBy5))
+                return input.ToString();
+
+            var fizz = divisibleBy3 ? "Fizz" : "";
+            var buzz = divisibleBy5 ? "Buzz" : "";
+            var fizzbuzz = string.Concat(fizz, buzz);
+            return fizzbuzz;
+
         }
     }
 }
--- FizzBuzz_2016_05_10/FizzBuzz_2016_05_10/TestFizzBuzzBuilder.cs
+++ FizzBuzz_2016_05_10/FizzBuzz_2016_05_10/TestFizzBuzzBuilder.cs
@@ -9,6 +9,14 @@ namespace FizzBuzz_2016_05_10
         [TestCase(2, "2")]
         [TestCase(3, "Fizz")]
         [TestCase(4, "4")]
+        [TestCase(5, "Buzz")]
+        [TestCase(6, "Fizz")]
+        [TestCase(9, "Fizz")]
+        [TestCase(10, "Buzz")]
+        [TestCase(15, "FizzBuzz")]
+        [TestCase(20, "Buzz")]
+        [TestCase(30, "FizzBuzz")]
+        [TestCase(75, "FizzBuzz")]
         public void Build_GivenInput_ShouldReturnResult(int input, string expected)
         {
             //---------------Set up test pack-------------------
