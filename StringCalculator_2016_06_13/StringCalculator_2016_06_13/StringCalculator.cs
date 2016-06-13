c77f78cfbd25a582e5f2ac48376908c509a118f8
LesediM <Lesedi.Motsemme@chillisoft.co.za>
2016-06-13 08:44:33 +0200

    13:10 with video

---
+++ StringCalculator_2016_06_13/StringCalculator_2016_06_13/StringCalculator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StringCalculator_2016_06_13
+{
+    internal class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+                return 0;
+
+            numbers = ResolveDelimiters(numbers);
+
+            if (numbers.Contains(","))
+            {
+                var split = numbers.Split(',', '\n');
+
+                CheckForNegatives(split);
+                var filterNumbers = split.Where(x => int.Parse(x) <= 1000);
+                return filterNumbers.Sum(x => int.Parse(x));
+            }
+
+            return int.Parse(numbers);
+        }
+
+        private static string ResolveDelimiters(string numbers)
+        {
+            if (numbers.StartsWith("//"))
+            {
+                var split = numbers.Split('\n');
+                var delimiterList = split[0].Replace("//", "").Split(new[] { '[', ']' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var numbersPart = split[1];
+
+                foreach (var delimiter in delimiterList)
+                {
+                    numbersPart = numbersPart.Replace(delimiter, ",");
+                }
+                return numbersPart;
+
+            }
+            return numbers;
+        }
+
+        private static void CheckForNegatives(string[] split)
+        {
+            var negatives = split.Where(x => int.Parse(x) < 0);
+            if (negatives.Any())
+                throw new ArgumentException("negatives not allowed: " + string.Join(",", negatives.ToArray()));
+        }
+    }
+}
