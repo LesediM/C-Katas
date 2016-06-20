629f83242b5fc94ea50dc8c90b30a1592d9473c4
LesediM <Lesedi.Motsemme@chillisoft.co.za>
2016-06-20 08:22:25 +0200

    Properly refactored string kata with video

---
+++ StringCalculator_2016_06_20/StringCalculator_2016_06_20/StringCalculator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StringCalculator_2016_06_20
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+                return 0;
+
+            numbers = ResolveDelimiters(numbers);
+
+            if (numbers.Contains(","))
+            {
+                var defaultDelimiters = new[] { ',', '\n' };
+                var split = numbers.Split(defaultDelimiters);
+                CheckForNegatives(split);
+                return GetNumbersBelow1000(split).Sum(x => int.Parse(x));
+            }
+
+            return int.Parse(numbers);
+        }
+
+        private static string ResolveDelimiters(string numbers)
+        {
+            if (numbers.StartsWith("//"))
+            {
+                var split = numbers.Split('\n');
+                var trimmings = new[] { '[', ']' };
+                var delimiters = split[0].Replace("//", "").Split(trimmings, StringSplitOptions.RemoveEmptyEntries);
+                var numbersPart = split[1];
+                foreach (var delimiter in delimiters)
+                {
+                    numbersPart = numbersPart.Replace(delimiter, ",");
+                }
+                return numbersPart;
+            }
+            return numbers;
+        }
+
+        private static IEnumerable<string> GetNumbersBelow1000(string[] split)
+        {
+            return split.Where(x => int.Parse(x) <= 1000);
+        }
+
+        private static void CheckForNegatives(string[] split)
+        {
+            var negatives = split.Where(x => int.Parse(x) < 0);
+            if (negatives.Any())
+            {
+                var message = "negatives not allowed: " + string.Join(",", negatives.ToArray());
+                throw new ArgumentException(message);
+            }
+        }
+    }
+}
