eaaf129772f4d1be72bded01f429998eba9773ac
LesediM <Lesedi.Motsemme@chillisoft.co.za>
2016-07-01 09:02:07 +0200

    new appraoch to roman numeral kata

---
+++ RomanNumeralsKata_2016_07_01/RomanNumeralsKata_2016_07_01/NumeralConverter.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RomanNumeralsKata_2016_07_01
+{
+    public class NumeralConverter
+    {
+        public int Convert(string numeralString)
+        {
+            var repeatingChars = new[] { "M", "C", "X", "I" };
+            var nonRepeatingChars = new[] { "D", "L", "V" };
+            ValidateInput(numeralString, repeatingChars, nonRepeatingChars);
+
+            numeralString = ResolveSubtractingNumerals(numeralString);
+            var digit = ProduceDigit(numeralString);
+            CheckResultsViolation(numeralString, digit);
+            return digit;
+        }
+
+        private static void ValidateInput(string numeralString, string[] repeatingChars, string[] nonRepeatingChars)
+        {
+            CheckForInValidNumerals(numeralString, repeatingChars, nonRepeatingChars);
+            CheckForRepeatViolations(numeralString, repeatingChars);
+            CheckForNonRepeatingCharsViolation(numeralString, nonRepeatingChars);
+        }
+
+        private static void CheckForInValidNumerals(string numeralString, string[] repeatingChars, string[] nonRepeatingChars)
+        {
+            var validNumerals = repeatingChars.Concat(nonRepeatingChars).ToArray();
+            var nonValidNumerals = numeralString.Where(n => !validNumerals.Contains(n.ToString()));
+            if (nonValidNumerals.Any())
+            {
+                var message = "Non repeat chars violation found: " + string.Join(",", nonValidNumerals.ToArray());
+                throw new ArgumentException(message);
+            }
+        }
+
+        private static void CheckForNonRepeatingCharsViolation(string numeralString, string[] nonRepeatingChars)
+        {
+            var nonRepeatingCharsViolation = nonRepeatingChars.Where(r => numeralString.Count(n => n.ToString() == r) > 1);
+            if (nonRepeatingCharsViolation.Any())
+            {
+                var message = "Non repeat chars violation found: " + string.Join(",", nonRepeatingCharsViolation.ToArray());
+                throw new ArgumentException(message);
+            }
+        }
+
+        private static void CheckForRepeatViolations(string numeralString, string[] repeatingChars)
+        {
+            var repeatLimitViolations = repeatingChars.Where(r => numeralString.Count(n => n.ToString() == r) > 3);
+            if (repeatLimitViolations.Any())
+            {
+                var message = "Repeat limit violated for: " + string.Join(",", repeatLimitViolations.ToArray());
+                throw new ArgumentException(message);
+            }
+        }
+
+        private static void CheckResultsViolation(string numeralString, int digit)
+        {
+            if (digit > 3000)
+                throw new ArgumentException("Sum cannot be greater than 3000: " + numeralString);
+        }
+
+        private static string ResolveSubtractingNumerals(string numeralString)
+        {
+            return numeralString
+                .Replace("IV", "IIII")
+                .Replace("IX", "VIIII")
+                .Replace("XL", "XXXX")
+                .Replace("XC", "LXXXX")
+                .Replace("CD", "CCCC")
+                .Replace("CM", "DCCCC");
+        }
+
+        private static int ProduceDigit(string numeralString)
+        {
+            var digit = 0;
+            foreach (var numeral in numeralString)
+            {
+                switch (numeral.ToString())
+                {
+                    case "I":
+                        digit += 1;
+                        break;
+                    case "V":
+                        digit += 5;
+                        break;
+                    case "X":
+                        digit += 10;
+                        break;
+                    case "L":
+                        digit += 50;
+                        break;
+                    case "C":
+                        digit += 100;
+                        break;
+                    case "D":
+                        digit += 500;
+                        break;
+                    case "M":
+                        digit += 1000;
+                        break;
+                }
+            }
+            return digit;
+        }
+    }
+}
---
+++ RomanNumeralsKata_2016_07_01/RomanNumeralsKata_2016_07_01/TestNumeralConverter.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace RomanNumeralsKata_2016_07_01
+{
+    [TestFixture]
+    public class TestNumeralConverter
+    {
+        [TestCase("I", 1)]
+        [TestCase("V", 5)]
+        [TestCase("X", 10)]
+        [TestCase("L", 50)]
+        [TestCase("C", 100)]
+        [TestCase("D", 500)]
+        [TestCase("M", 1000)]
+        public void Convert_GivenSingleNumeralString_ShouldReturnDigit(string numerlalString, int expected)
+        {
+            //---------------Set up test pack-------------------
+            var converter = CreateConverter();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = converter.Convert(numerlalString);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void Convert_GivenNumeralStringCombination_ShouldReturnDigit()
+        {
+            //---------------Set up test pack-------------------
+            var numeralString = "VII";
+            var expected = 7;
+            var converter = CreateConverter();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = converter.Convert(numeralString);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+
+        [TestCase("IV", 4)]
+        [TestCase("IX", 9)]
+        [TestCase("XL", 40)]
+        [TestCase("XC", 90)]
+        [TestCase("CD", 400)]
+        [TestCase("CM", 900)]
+        public void Convert_GivenSingleSubtractingNumeralString_ShouldReturnDigit(string numerlalString, int expected)
+        {
+            //---------------Set up test pack-------------------
+            var converter = CreateConverter();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = converter.Convert(numerlalString);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+        [Test]
+        public void Convert_GivenNumeralString_ShouldReturnExceptionWhenSumGreaterThan3000()
+        {
+            //---------------Set up test pack-------------------
+            var numeralString = "MMMI";
+            var expected = "Sum cannot be greater than 3000: MMMI";
+            var converter = CreateConverter();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = Assert.Throws<ArgumentException>(() => converter.Convert(numeralString));
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results.Message);
+        }
+        [Test]
+        public void Convert_GivenNumeralStringWithRepeatLimitViolated_ShouldReturnException()
+        {
+            //---------------Set up test pack-------------------
+            var numeralString = "VIIII";
+            var expected = "Repeat limit violated for: I";
+            var converter = CreateConverter();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = Assert.Throws<ArgumentException>(() => converter.Convert(numeralString));
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results.Message);
+        }
+        [Test]
+        public void Convert_GivenNumeralStringWithManyRepeatLimitViolated_ShouldReturnException()
+        {
+            //---------------Set up test pack-------------------
+            var numeralString = "CCCCVIIII";
+            var expected = "Repeat limit violated for: C,I";
+            var converter = CreateConverter();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = Assert.Throws<ArgumentException>(() => converter.Convert(numeralString));
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results.Message);
+        }
+        [Test]
+        public void Convert_GivenNumeralStringWithNonRepeatCharsViolated_ShouldReturnException()
+        {
+            //---------------Set up test pack-------------------
+            var numeralString = "VVIII";
+            var expected = "Non repeat chars violation found: V";
+            var converter = CreateConverter();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = Assert.Throws<ArgumentException>(() => converter.Convert(numeralString));
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results.Message);
+        }
+        [Test]
+        public void Convert_GivenNumeralStringWithManyNonRepeatCharsViolated_ShouldReturnException()
+        {
+            //---------------Set up test pack-------------------
+            var numeralString = "DDVVIII";
+            var expected = "Non repeat chars violation found: D,V";
+            var converter = CreateConverter();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = Assert.Throws<ArgumentException>(() => converter.Convert(numeralString));
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results.Message);
+        }
+        [Test]
+        public void Convert_GivenNumeralStringWithStrangeChars_ShouldReturnException()
+        {
+            //---------------Set up test pack-------------------
+            var numeralString = "DAVIII";
+            var expected = "Non repeat chars violation found: A";
+            var converter = CreateConverter();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = Assert.Throws<ArgumentException>(() => converter.Convert(numeralString));
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results.Message);
+        }
+        [Test]
+        public void Convert_GivenNumeralStringWithManyStrangeChars_ShouldReturnException()
+        {
+            //---------------Set up test pack-------------------
+            var numeralString = "DABVIII";
+            var expected = "Non repeat chars violation found: A,B";
+            var converter = CreateConverter();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = Assert.Throws<ArgumentException>(() => converter.Convert(numeralString));
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results.Message);
+        }
+
+        [TestCase("MMDCCLI", 2751)]
+        [TestCase("CDXLVIII", 448)]
+        [TestCase("CCCLXIX", 369)]
+        public void Convert_GivenComplexNumeralString_ShouldReturnDigit(string numeralString, int expected)
+        {
+            //---------------Set up test pack-------------------
+            var converter = CreateConverter();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = converter.Convert(numeralString);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+
+
+        private static NumeralConverter CreateConverter()
+        {
+            return new NumeralConverter();
+        }
+    }
+}
