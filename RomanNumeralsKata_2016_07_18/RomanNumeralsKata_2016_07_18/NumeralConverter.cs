0e970b746feede5bb27c4bd6aafc6b1f4e034655
LesediM <Lesedi.Motsemme@chillisoft.co.za>
2016-07-18 08:20:35 +0200

    around 25 to 28 minutes duration

---
+++ RomanNumeralsKata_2016_07_18/RomanNumeralsKata_2016_07_18/NumeralConverter.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace RomanNumeralsKata_2016_07_18
+{
+    public class NumeralConverter
+    {
+        public int Convert(string numeral)
+        {
+            CheckForStrangeNumerals(numeral);
+            CheckForNonRepeatingCharsRepeated(numeral);
+            CheckForRepeatViolations(numeral);
+            numeral = ResolveSubtractingNumerals(numeral);
+            return GetSum(numeral);
+        }
+
+        private static void CheckForStrangeNumerals(string numeral)
+        {
+            var romanNumerals = new[] { "I", "V", "X", "L", "C", "D", "M" };
+            var strangeNumerals = numeral.Where(n => !romanNumerals.Contains(n.ToString()));
+            if (strangeNumerals.Any())
+            {
+                var message = "Numerals cannot be rendered: " + string.Join(",", strangeNumerals.ToArray());
+                throw new ArgumentException(message);
+            }
+        }
+
+        private int GetSum(string numeral)
+        {
+            var sum = numeral.Sum(x => GetDigit(x.ToString()));
+            if (sum > 3000)
+                throw new ArgumentException("Numerals cannot be above 3000");
+            return sum;
+        }
+
+        private static void CheckForNonRepeatingCharsRepeated(string numeral)
+        {
+            var nonRepeatingNumerals = new[] { "D", "L", "V" };
+            var nonRepeatViolation = nonRepeatingNumerals.Where(r => numeral.Count(n => n.ToString() == r) > 1);
+            if (nonRepeatViolation.Any())
+            {
+                var message = "Numerals cannot be repeated: " + string.Join(",", nonRepeatViolation.ToArray());
+                throw new ArgumentException(message);
+            }
+        }
+
+        private static void CheckForRepeatViolations(string numeral)
+        {
+            var repeatableNumerals = new[] { "M", "C", "X", "I" };
+            var repeatLimitViolation = repeatableNumerals.Where(r => numeral.Count(n => n.ToString() == r) > 3);
+            if (repeatLimitViolation.Any())
+            {
+                var message = "Numerals cannot be repeated more than 3 times: " + string.Join(",", repeatLimitViolation.ToArray());
+                throw new ArgumentException(message);
+            }
+        }
+
+        private static string ResolveSubtractingNumerals(string numeral)
+        {
+            return numeral
+                .Replace("IV", "IIII")
+                .Replace("IX", "VIIII")
+                .Replace("XL", "XXXX")
+                .Replace("XC", "LXXXX")
+                .Replace("CD", "CCCC")
+                .Replace("CM", "DCCCC");
+        }
+
+        private int GetDigit(string numeral)
+        {
+            switch (numeral)
+            {
+                case "I":
+                    return 1;
+                case "V":
+                    return 5;
+                case "X":
+                    return 10;
+                case "L":
+                    return 50;
+                case "C":
+                    return 100;
+                case "D":
+                    return 500;
+                case "M":
+                    return 1000;
+            }
+            return -1;
+        }
+    }
+}
---
+++ RomanNumeralsKata_2016_07_18/RomanNumeralsKata_2016_07_18/TestNumeralConverter.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace RomanNumeralsKata_2016_07_18
+{
+    [TestFixture]
+    public class TestNumeralConverter
+    {
+        [TestCase("I", 1)]
+        [TestCase("V", 5)]
+        [TestCase("X", 10)]
+        [TestCase("L", 50)]
+        [TestCase("C", 100)]
+        [TestCase("D", 500)]
+        [TestCase("M", 1000)]
+        public void Convert_GivenNumeralString_ShouldReturnDigit(string numeral, int expected)
+        {
+            //---------------Set up test pack-------------------
+            var converter = CreateConverter();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = converter.Convert(numeral);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+        [Test]
+        public void Convert_GivenSimpleMixedNumeralString_ShouldReturnDigit()
+        {
+            //---------------Set up test pack-------------------
+            var numeral = "VII";
+            var expected = 7;
+            var converter = CreateConverter();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = converter.Convert(numeral);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+
+
+        [TestCase("IV", 4)]
+        [TestCase("IX", 9)]
+        [TestCase("XL", 40)]
+        [TestCase("XC", 90)]
+        [TestCase("CD", 400)]
+        [TestCase("CM", 900)]
+        public void Convert_GivenSubtractingNumeralString_ShouldReturnDigit(string numeral, int expected)
+        {
+            //---------------Set up test pack-------------------
+            var converter = CreateConverter();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = converter.Convert(numeral);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void Convert_GivenNumeralStringWithRepitionAboveThree_ShouldReturnException()
+        {
+            //---------------Set up test pack-------------------
+            var numeral = "VIIIII";
+            var expected = "Numerals cannot be repeated more than 3 times: I";
+            var converter = CreateConverter();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = Assert.Throws<ArgumentException>(() => converter.Convert(numeral));
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results.Message);
+        }
+        [Test]
+        public void Convert_GivenNumeralStringWithManyRepitionAboveThree_ShouldReturnException()
+        {
+            //---------------Set up test pack-------------------
+            var numeral = "XXXXVIIIII";
+            var expected = "Numerals cannot be repeated more than 3 times: X,I";
+            var converter = CreateConverter();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = Assert.Throws<ArgumentException>(() => converter.Convert(numeral));
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results.Message);
+        }
+        [Test]
+        public void Convert_GivenNumeralStringWithNonRepeatinCharsRepeated_ShouldReturnException()
+        {
+            //---------------Set up test pack-------------------
+            var numeral = "VVII";
+            var expected = "Numerals cannot be repeated: V";
+            var converter = CreateConverter();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = Assert.Throws<ArgumentException>(() => converter.Convert(numeral));
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results.Message);
+        }
+        [Test]
+        public void Convert_GivenNumeralStringWithManyNonRepeatinCharsRepeated_ShouldReturnException()
+        {
+            //---------------Set up test pack-------------------
+            var numeral = "DDVVII";
+            var expected = "Numerals cannot be repeated: D,V";
+            var converter = CreateConverter();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = Assert.Throws<ArgumentException>(() => converter.Convert(numeral));
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results.Message);
+        }
+        [Test]
+        public void Convert_GivenNumeralStringReturnDigitAboveThreeThousand_ShouldReturnException()
+        {
+            //---------------Set up test pack-------------------
+            var numeral = "MMMI";
+            var expected = "Numerals cannot be above 3000";
+            var converter = CreateConverter();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = Assert.Throws<ArgumentException>(() => converter.Convert(numeral));
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results.Message);
+        }
+        [Test]
+        public void Convert_GivenNumeralStringWithStrangeChars_ShouldReturnException()
+        {
+            //---------------Set up test pack-------------------
+            var numeral = "MMMZI";
+            var expected = "Numerals cannot be rendered: Z";
+            var converter = CreateConverter();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = Assert.Throws<ArgumentException>(() => converter.Convert(numeral));
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results.Message);
+        }
+        [Test]
+        public void Convert_GivenNumeralStringWithManyStrangeChars_ShouldReturnException()
+        {
+            //---------------Set up test pack-------------------
+            var numeral = "MMMZGI";
+            var expected = "Numerals cannot be rendered: Z,G";
+            var converter = CreateConverter();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = Assert.Throws<ArgumentException>(() => converter.Convert(numeral));
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results.Message);
+        }
+
+
+        [TestCase("MMDCCLI", 2751)]
+        [TestCase("CDXLVIII", 448)]
+        [TestCase("CCCLXIX", 369)]
+        public void Convert_GivenNumeralStringWithComplexCombination_ShouldReturnDigit(string numeral, int expected)
+        {
+            //---------------Set up test pack-------------------
+            var converter = CreateConverter();
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var results = converter.Convert(numeral);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+
+        private static NumeralConverter CreateConverter()
+        {
+            return new NumeralConverter();
+        }
+    }
+}
